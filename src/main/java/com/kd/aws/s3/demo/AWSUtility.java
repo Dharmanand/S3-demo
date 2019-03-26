f15f36e4372a5cbc6d51c5a9f449176c15cd50e2
Dharmanand <kumar.kd20@gmail.com>
2019-03-26 08:38:56 +0530

    added the credentials to properties file

--- src/main/java/com/kd/aws/s3/demo/AWSUtility.java
+++ src/main/java/com/kd/aws/s3/demo/AWSUtility.java
@@ -1,5 +1,9 @@
 package com.kd.aws.s3.demo;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
@@ -9,12 +13,20 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
 public class AWSUtility {
 
+	private static Properties properties = new Properties();
+	static {
+		try {
+			properties.load(new FileReader("D:/lara-soft/installers/windows/aws/credentials.properties"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 	public static AmazonS3 getS3CredentialObj() {
-		AWSCredentials credentials = new BasicAWSCredentials("accessKey",
-				"secretKey");
-		
-		AmazonS3 s3 = AmazonS3ClientBuilder.standard()
-				.withCredentials(new AWSStaticCredentialsProvider(credentials))
+		AWSCredentials credentials = new BasicAWSCredentials(properties.getProperty("accessKey"),
+				properties.getProperty("secretKey"));
+
+		AmazonS3 s3 = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(credentials))
 				.withRegion(Regions.US_EAST_1).build();
 		return s3;
 	}
